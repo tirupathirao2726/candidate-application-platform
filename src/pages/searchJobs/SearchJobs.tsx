30c0139a46c4cff8b41d74d18359e11673ad0f8c
tirupathirao0201 <124332583+tirupathirao0201@users.noreply.github.com>
2024-05-05 11:35:50 +0530

    added grip with responsive

--- src/pages/searchJobs/SearchJobs.tsx
+++ src/pages/searchJobs/SearchJobs.tsx
@@ -4,43 +4,47 @@ import fetchJobList from "../../services/fetchJobs";
 import { useDispatch, useSelector } from "react-redux";
 import { addJobs } from "../../reducerSlices/jobListingSlice";
 import { updateIsLoading } from "../../reducerSlices/loaderSlice";
+import { Grid } from "@mui/material";
+import './SearchJobsStyles.css';
 const SearchJobs = () => {
   const dispatch = useDispatch();
-  const { currentJobs } = useSelector((state: any) => state.jobs);
-  console.log("jobss",currentJobs);
+  const { currentJobs, limit,currentOffset } = useSelector((state: any) => state.jobs);
   useEffect(() => {
     dispatch(updateIsLoading(true));
-    fetchJobList(10, 0)
+    fetchJobList(limit, currentOffset)
       .then((res: any) => {
-        console.log("result", res);
         dispatch(addJobs(res));
       })
       .catch((err) => {
         console.log("err--", err);
       });
     dispatch(updateIsLoading(false));
-  }, []);
+  }, [limit,currentOffset,dispatch]);
   return (
-    <div>
-      <h2>hii</h2>
-      {currentJobs && currentJobs?.length > 0 && 
-        currentJobs?.map((job: any) => (
-          <JobCard
-            jobId={job.jdUid}
-            companyLogo={job.logoUrl}
-            companyName={job.companyName}
-            jobTitle={job.jobRole}
-            jobDescription={job.jobDetailsFromCompany}
-            jobLink={job.jdLink}
-            maxSalary={job.maxJdSalary}
-            minSalary={job.minJdSalary}
-            currencyCode={job.salaryCurrencyCode}
-            location={job.location}
-            minExperience={job.minExp}
-            maxExperience={job.maxExp}
-          />
+    <section  className="jobslist-section"> 
+      {
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {currentJobs.map((job:any) => (
+          <Grid item xs={2} sm={4} md={4} key={job.jdUid}>
+            <JobCard
+              jobId={job.jdUid}
+              companyLogo={job.logoUrl}
+              companyName={job.companyName}
+              jobTitle={job.jobRole}
+              jobDescription={job.jobDetailsFromCompany}
+              jobLink={job.jdLink}
+              maxSalary={job.maxJdSalary}
+              minSalary={job.minJdSalary}
+              currencyCode={job.salaryCurrencyCode}
+              location={job.location}
+              minExperience={job.minExp}
+              maxExperience={job.maxExp}
+            />
+          </Grid>
         ))}
-    </div>
+      </Grid>
+      }
+    </section>
   );
 };
 
--- src/reducerSlices/jobListingSlice.ts
+++ src/reducerSlices/jobListingSlice.ts
@@ -1,18 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+type InitialJobState  = {
+  currentJobs:any[];
+  limit:number;
+  currentOffset: number;
+  totalJobs: number;
+}
+
+const initialState:InitialJobState = {
+  currentJobs:[],
+  limit:10,
+  currentOffset:0,
+  totalJobs:0,
+}
 const jobsListSlice = createSlice({
     name: "jobs",
-    initialState:{
-      currentJobs:[],
-      limit:10,
-      currentOffset:0,
-      totalJobs:0,
-    },
+    initialState:initialState,
     reducers: {
-        addJobs: (state:any, action:any) => {
+        addJobs: (state:InitialJobState, action:any) => {
             if(action.payload){
               return {...state,currentJobs:[...state.currentJobs,...action.payload.jdList],totalJobs:action.payload.totalCount}
             }
+        },
+        reset:() => {
+          return initialState
         }
     },
 });
