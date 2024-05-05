750061970ce3fcbfc622daf5572c7c81cf6cd50c
tirupathirao0201 <124332583+tirupathirao0201@users.noreply.github.com>
2024-05-05 21:46:35 +0530

    feat:handled null values

--- src/common/components/jobCard/JobCard.tsx
+++ src/common/components/jobCard/JobCard.tsx
@@ -15,31 +15,53 @@ const JobCard = ({
   minExperience,
   maxExperience,
 }: IJobCard) => {
-  return <Paper elevation={2} className="jobcard" key={jobId}>
-    <div className="jobcard-header">
-      <div className="company-info">
-        <div className="company-logo">
-          <img src={companyLogo} alt="logo" />
-        </div>
-        <div className="company-details">
-          <a className="company-name" href={jobLink}>{companyName}</a>
-          <h3 className="role-offered">{jobTitle}</h3>
-          <p className="location">{location}</p>
+  return (
+    <Paper elevation={2} className="jobcard" key={jobId}>
+      <div className="jobcard-header">
+        <div className="company-info">
+          <div className="company-logo">
+            <img src={companyLogo} alt="logo" />
+          </div>
+          <div className="company-details">
+            <a className="company-name" href={jobLink}>
+              {companyName}
+            </a>
+            <h3 className="role-offered">{jobTitle}</h3>
+            <p className="location">{location}</p>
+          </div>
         </div>
       </div>
-    </div>
-    <div className="job-card-body">
-      <p className="estimated-salary">{`Estimated Salary: ${currencyCode} ${minSalary} - ${maxSalary} LPA`}</p>
-      <div className="about-role">
+      <div className="job-card-body">
+        {minSalary && maxSalary ? (
+          <p className="estimated-salary">{`Estimated Salary: ${currencyCode} ${minSalary} - ${maxSalary} LPA`}</p>
+        ) : minSalary != null && maxSalary == null ? (
+          <p className="estimated-salary">{`Estimated Salary: ${currencyCode} ${minSalary} LPA`}</p>
+        ) : minSalary == null && maxSalary != null ? (
+          <p className="estimated-salary">{`Estimated Salary: ${currencyCode} ${maxSalary} LPA`}</p>
+        ) : (
+          <></>
+        )}
+        <div className="about-role">
           <h3 className="about-role-heading">About Role</h3>
           <p className="role-description">{jobDescription}</p>
+        </div>
+        {(minExperience != null || maxExperience != null) && (
+          <p className="experience-heading">Minimum Experience</p>
+        )}
+        {minExperience && maxExperience ? (
+          <p className="experience-years">{`${minExperience} - ${maxExperience} years`}</p>
+        ) : minExperience != null && maxExperience == null ? (
+          <p className="experience-years">{`${minExperience} years`}</p>
+        ) : minExperience == null && maxExperience != null ? (
+          <p className="experience-years">{`${maxExperience} years`}</p>
+        ) : (
+          <></>
+        )}
       </div>
-        <p className="experience-heading">Minimum Experience</p>
-        <p className="experience-years">{`${minExperience} - ${maxExperience} years`}</p>
-    </div>
-    <div className="job-card-footer">
-      <button className="apply-button">Easy Apply</button>
-    </div>
-  </Paper>;
+      <div className="job-card-footer">
+        <button className="apply-button">Easy Apply</button>
+      </div>
+    </Paper>
+  );
 };
 export default JobCard;
